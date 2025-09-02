f92f2c357d63b30419771b99626acd2c48895cf8
max feinberg <me@thelastinu.it>
2025-09-02 20:40:18 +0300

    Initial commit

---
+++ lib/o_mana.ex
@@ -0,0 +1,18 @@
+defmodule OMana do
+  @moduledoc """
+  Documentation for `OMana`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OMana.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/o_mana_test.exs
@@ -0,0 +1,8 @@
+defmodule OManaTest do
+  use ExUnit.Case
+  doctest OMana
+
+  test "greets the world" do
+    assert OMana.hello() == :world
+  end
+end
